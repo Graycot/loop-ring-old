cf780b35dce81461f8b87dc371246d1a184d47c0
Graycot <ggraycot@gmail.com>
2022-11-07 00:15:21 -0500

    remove old code

--- webring/webring.js
+++ webring/webring.js
@@ -33,13 +33,7 @@ function sites(data) {
   var referrerURL = document.referrer;
   var referrerURLReplace = referrerURL.replace(/http(s)?(:)?(\/\/)?|(\/\/)?(www\.)?/g, "");
 
-
-  //var dummy = "https://vaea-v.neocities.org/home/"
-  //var test = dummy.match(/^(?:https?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?([^:\/\n]+)/igm, "");
-  //var test2 = test[0].replace(/http(s)?(:)?(\/\/)?|(\/\/)?(www\.)?/g, "");
-  //console.log(test2);
-
-  //find referrer site in member list
+  // find referrer member site in member list.
   for (i = 0; i < data.webringSites.length; i++) {
     siteURLRaw = data.webringSites[i].siteURL;
     siteURLMatch = siteURLRaw.match(/^(?:https?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?([^:\/\n]+)/igm, "");
